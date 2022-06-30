4bc013fb899f0bd94b358ac5714a538edfcc6b1f
TranTam <tamvaa1306@gmail.com>
2022-06-30 17:37:32 +0700

    footer

---
+++ components/footer.js
@@ -0,0 +1,21 @@
+Vue.component("app-footer", {
+  template: ` <footer class="footer">
+          <div class="footer_left">
+            <button class="prev">
+              <img src="./assets/svg/arrow-left.svg" alt="" />Prev
+            </button>
+            <button class="next">
+              Next
+              <img src="./assets/svg/arrow-left.svg" alt="" />
+            </button>
+          </div>
+          <div class="footer_right">
+            <button class="cancel">Cancel</button>
+            <button class="save">Save</button>
+          </div>
+        </footer>`,
+
+  data() {
+    return {};
+  },
+});
