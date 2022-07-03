52e0e3437aace5d96a6d7d403cf00d1941685ac3
TranTam <tamvaa1306@gmail.com>
2022-07-03 20:59:04 +0700

    comment product.js

--- components/product.js
+++ components/product.js
@@ -6,7 +6,6 @@ export default Vue.component("app-product", {
                 <label for="numberSelected" v-if="selectedProducts.length==0">Product</label>
                       <label for="numberSelected" v-else-if="selectedProducts.length==1">1 Product seleted</label>
                             <label for="numberSelected" v-else>{{selectedProducts.length}} Products selected</label>
-                
               </div>
               <span class="products_header-price">Price</span>
             </div>
@@ -103,12 +102,13 @@ export default Vue.component("app-product", {
       localStorage.setItem(
         "selectedProducts",
         JSON.stringify(this.selectedProducts)
-      );
+      ); // Save Data to LocalStorage
     },
   },
   created() {
     this.selectedProducts =
       JSON.parse(localStorage.getItem("selectedProducts")) || [];
+    // Get Data from LocalStorage, if LocalStorage empty => []
   },
   mounted() {
     // check selected products
@@ -121,13 +121,14 @@ export default Vue.component("app-product", {
     }
 
     if (this.selectedProducts.length > 0) {
-      // If have >0 product => check
+      // If have > 0 product => check input
       this.countedProducts = true;
     }
   },
   watch: {
     searchValue() {
       if (this.searchValue.length > 0) {
+        // Do searching when type
         this.isSearching = true;
         this.productList = this.productList.filter((product) => {
           return (
@@ -139,12 +140,15 @@ export default Vue.component("app-product", {
         });
       }
       this.selectedProducts.forEach((item) => {
+        // Check input after search and filt
         setTimeout(function () {
           document.getElementById(String(item)).checked = true;
         }, 10);
       });
     },
+
     aToZSort() {
+      // Sort by product name
       if (this.aToZSort == true) {
         this.productList = this.productList.sort((a, b) =>
           a.name.localeCompare(b.name)
@@ -155,12 +159,18 @@ export default Vue.component("app-product", {
           .reverse();
       }
     },
+
     productList() {
       this.maxPage = Math.ceil(this.productList.length / 10);
+      // Calculate the max page, 10 items/page
+      // Example: 15 items => 2 pages, 122 items => 13 pages
     },
+
     currentPage() {
       this.firstItemIndex = (this.currentPage - 1) * 10;
       this.lastItemIndex = this.currentPage * 10;
+      // Items of nth page
+      // Example: 2nd page: first item index = 10, last item index = 19
       if (this.currentPage == this.maxPage) {
         this.stopNext = true;
       } else {
@@ -171,8 +181,9 @@ export default Vue.component("app-product", {
       } else if (this.currentPage <= 1) {
         this.stopPrev = true;
       }
-      // check selected products
+      // Disable prev or next button when reach limit
       if (this.selectedProducts.length > 0) {
+        // Check selected products after change page
         this.selectedProducts.forEach((item) => {
           setTimeout(function () {
             document.getElementById(String(item)).checked = true;
@@ -180,15 +191,19 @@ export default Vue.component("app-product", {
         });
       }
     },
+
     selectedProducts() {
+      // Check input when have selected product in selectedProducts array
       if (this.selectedProducts.length > 0) {
         this.hasProducts = true;
       } else {
         this.hasProducts = false;
       }
     },
+
     hasProducts() {
       if (this.hasProducts == false) {
+        // Uncheck all selected product and reset selectedProducts array
         async function removeItems(arr) {
           return await arr.map((item) => {
             return (document.getElementById(String(item)).checked = false);
@@ -197,6 +212,7 @@ export default Vue.component("app-product", {
         this.selectedProducts = removeItems(this.selectedProducts);
         this.selectedProducts = [];
       } else {
+        // Select all products
         if (this.selectedProducts.length == 0) {
           this.productList.forEach((item) => {
             this.selectedProducts.push(item.id);
